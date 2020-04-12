35e25a4759e7da58b771369f08037634b1403558
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-12 15:12:02 +0900

    router部分の書き換え

--- app/app.js
+++ app/app.js
@@ -11,12 +11,8 @@ app.use(bodyParser.json());
 let port = process.env.PORT || 3000;
 
 //GET 3000/api/v1
-app.get("/api/v1/" ,function(req,res){
-  res.json({
-    message:"Hello, World"
-  })
-});
-
+let router = require("/routes/v1/");
+app.use("/api/v1/",router);
 // 起動
 app.listen(port);
 console.log("起動しました" + port);
